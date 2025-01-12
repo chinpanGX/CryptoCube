e19b9d5c233fc52a298d4bd2fd728432018b9159
ちんぱん <52692512+chinpanGX@users.noreply.github.com>
2025-01-12 20:57:29 +0900

    プロップの作成、ShaderGraph、UnityChanの導入 (#2)
    * シールドのShaderGraphとマテリアルを追加
    
    * UnityChan、アニメーションの追加
    
    * 衝突処理の共通化
    
    * トラップの線を追加

---
+++ CryptoCubeUnityProject/Assets/App/Scripts/Ingame/Presentation/Props/TrapLine.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using R3;
+using UnityEngine;
+
+namespace App.InGame.Presentation.Props
+{
+    internal class TrapLine : MonoBehaviour
+    {
+        public void Construct(Action action)
+        {
+            transform.OnTriggerEnterToPlayer()
+                .Subscribe(_ => action.Invoke())
+                .RegisterTo(destroyCancellationToken);
+        }
+    }
+}
---
+++ CryptoCubeUnityProject/Assets/App/Scripts/Ingame/Presentation/Props/TriggerExtensions.cs
@@ -0,0 +1,15 @@
+﻿using R3;
+using R3.Triggers;
+using UnityEngine;
+
+namespace App.InGame.Presentation.Props
+{
+    internal static class TriggerExtensions
+    {
+        public static Observable<Collider> OnTriggerEnterToPlayer(this Transform transform)
+        {
+            return transform.OnTriggerEnterAsObservable()
+                .Where(x => x.gameObject.CompareTag("Player"));
+        }
+    }
+}
